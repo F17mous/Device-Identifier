df0af4aa372cbd77d33c6fa5a8264863de1d2730
F17mous <F17mous@gmail.com>
2017-06-11 23:12:47 +0200

    Create identifier.php

--- identifier.php
+++ identifier.php
@@ -9,7 +9,6 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $port = $_SERVER['REMOTE_PORT'];
 $agent = $_SERVER['HTTP_USER_AGENT'];
 $ref = $_SERVER['HTTP_REFERER'];
-$date = new DateTime('NOW')->format("y:m:d h:i:s");
 $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 
  
@@ -20,7 +19,6 @@ fwrite($fh, 'IP Address: '."".$ip ."\n");
 fwrite($fh, 'Hostname: '."".$hostname ."\n");
 fwrite($fh, 'Port Number: '."".$port ."\n");
 fwrite($fh, 'User Agent: '."".$agent ."\n");
-fwrite($fh, 'User Agent: '."".$date ."\n");
 fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n");
 fclose($fh);
 ?>
